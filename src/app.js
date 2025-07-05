4bc1b10c32b478d05e6f92311f247cf0c4328558
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-07-05 13:18:38 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "corsr"
+import cors from "cors"
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -10,10 +10,10 @@ app.use(cors({
 }))
 
 //Used to import data in json format with 16kb size limit
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
 
 //used encode url links with special characters such as %20 , + .the extended allows for further nesting eg objects
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
 //allows to store files or data on server in public folder created in the main folder
 app.use(express.static("public"))
@@ -21,4 +21,11 @@ app.use(express.static("public"))
 //it allows CRUD operations on users secure cookies
 app.use(cookieParser())
 
-export {app}
+//Routes import
+import userRouter from './routes/user.routes.js'
+
+
+//Route declaration
+app.use("/api/v1/users", userRouter) //example url https://localhost:8000/api/v1/users/'when ever user goes in this route then he will be redirected on logij or sign up page automatically';
+
+export { app }
